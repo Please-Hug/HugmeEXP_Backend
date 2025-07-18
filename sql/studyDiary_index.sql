5f4e5d24edfcfd36483532c71d3aaf828efa0bc6
Roysung0625 <roy06255@gmail.com>
2025-07-19 04:16:15 +0900

    fix: 쿼리 n-gram 파서 사용하게 수정

--- sql/studyDiary_index.sql
+++ sql/studyDiary_index.sql
@@ -5,4 +5,4 @@ CREATE INDEX idx_study_diary_created_at ON study_diary(created_at);
 
 -- like 쿼리 개선 인덱스
 ALTER TABLE study_diary
-ADD FULLTEXT INDEX ft_index_title_content (title, content);
+ADD FULLTEXT INDEX ft_index_title_content (title, content) WITH PARSER ngram;
