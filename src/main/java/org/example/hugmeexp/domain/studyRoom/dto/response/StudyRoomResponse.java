a66cc735f886247d1a27e22d73b14827a5d921ff
SeongJun98 <seongjunin98@gmail.com>
2025-07-24 15:22:44 +0900

    refactor(studyRoom): 필드에 final 추가

--- src/main/java/org/example/hugmeexp/domain/studyRoom/dto/response/StudyRoomResponse.java
+++ src/main/java/org/example/hugmeexp/domain/studyRoom/dto/response/StudyRoomResponse.java
@@ -1,15 +1,15 @@
 package org.example.hugmeexp.domain.studyRoom.dto.response;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 /**
  * 스터디 룸 정보 응답을 위한 DTO 입니다.
  */
 @Getter
 @Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class StudyRoomResponse {
-    private Long id;
-    private String name;
-    private Integer maxNum;
+    private final Long id;
+    private final String name;
+    private final Integer maxNum;
 }
