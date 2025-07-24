8cf8e1e264d8de57c9175b9236eafe9311072311
SeongJun98 <seongjunin98@gmail.com>
2025-07-24 16:57:21 +0900

    feat(studyRoom): 스터디 홀 수정 및 삭제 비즈니스 로직 구현
    - StudyHall 엔티티에 수정(update) 및 논리적 삭제(delete) 메서드 추가
    - StudyHallService에 관련 비즈니스 로직 추가
    - 논리적 삭제를 반영하여 조회 로직 수정

--- src/main/java/org/example/hugmeexp/domain/studyRoom/entity/StudyHall.java
+++ src/main/java/org/example/hugmeexp/domain/studyRoom/entity/StudyHall.java
@@ -2,6 +2,7 @@ package org.example.hugmeexp.domain.studyRoom.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.example.hugmeexp.domain.studyRoom.dto.request.StudyHallRequest;
 import org.example.hugmeexp.global.entity.BaseEntity;
 
 import java.time.LocalDateTime;
@@ -13,6 +14,8 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StudyHall extends BaseEntity {
+
+    private boolean isDeleted = false;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,4 +42,26 @@ public class StudyHall extends BaseEntity {
 
     @OneToMany(mappedBy = "studyHall", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<StudyRoom> studyRooms;
+
+    /**
+     * 스터디 홀 정보를 수정하는 메서드입니다.
+     */
+    public void update(StudyHallRequest requestDto) {
+        this.name = requestDto.getName();
+        this.description = requestDto.getDescription();
+        this.simpleAddress = requestDto.getSimpleAddress();
+        this.address = requestDto.getAddress();
+        this.latitude = requestDto.getLatitude();
+        this.longitude = requestDto.getLongitude();
+        this.thumbnail = requestDto.getThumbnail();
+        this.openTime = requestDto.getOpenTime();
+        this.closeTime = requestDto.getCloseTime();
+    }
+
+    /**
+     * 스터디 홀을 논리적으로 삭제 처리하는 메서드입니다.
+     */
+    public void delete() {
+        this.isDeleted = true;
+    }
 }
